ec9f8f4d0d968a9a5449d7c3c1f7aab142475d8d
acr1991 <acr1991@live.nl>
2020-02-01 15:21:44 +0100

    Update of project progress

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -5,15 +5,30 @@
 //X Hero object should have an inventory property. The value of inventory should be an empty array
 //X Hero object should have an health property. The value health should be the number 10
 //X Hero object should have a weapon property. The value of weapon is an object with a type property which is a string and damage property which is 2
-//X console.log("Connetion Test");
-// Functions `rest, pickUpItem and equipWeapon` are all defined
+//console.log("Connetion Test");
+//X Functions `rest, pickUpItem and equipWeapon` are all defined
+// And (re)assigns the health property of person to the value 10
+//
 
-const hero = {
-  name: `Alexander`,
+//HERO OBJECT
+let hero = {
+  name: `Alexander the Great`,
   heroic: true,
   inventory: [],
   health: 10,
-  weapon: { type: `Bazooka`, damage: 2 }
+  weapon: { type: `Sword`, damage: 2 }
 };
-
 //console.log(hero);
+
+//FUNCTIONS
+function rest(person) {
+  const lifeForce = person.health;
+  if (lifeForce === 10) {
+    window.alert("You have full health");
+  }
+  person.health = 10;
+  return person;
+}
+console.log(rest);
+function pickUpItem(params) {}
+function equipWeapon(params) {}
